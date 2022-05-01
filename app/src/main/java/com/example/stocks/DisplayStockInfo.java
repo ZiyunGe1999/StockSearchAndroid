9328ca0131fe171739e4e751e83faaadaf79437f
Ziyun Ge <ziyunge1999@gmail.com>
2022-04-30 19:10:01 -0700

    set up basic stock info page

--- app/src/main/java/com/example/stocks/DisplayStockInfo.java
+++ app/src/main/java/com/example/stocks/DisplayStockInfo.java
@@ -2,9 +2,22 @@ package com.example.stocks;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DisplayStockInfo extends AppCompatActivity {
     @Override
@@ -16,8 +29,39 @@ public class DisplayStockInfo extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
+        setTitle(message);
+
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.stockText);
         textView.setText(message);
+
+        TextView responseTextView = findViewById(R.id.responseText);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://stocksearchnodejs.wl.r.appspot.com/api/v1/stock/profile2?symbol=AAPL";
+
+        // Request a json response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            responseTextView.setText("Response: " + response.getString("name"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
     }
 }
