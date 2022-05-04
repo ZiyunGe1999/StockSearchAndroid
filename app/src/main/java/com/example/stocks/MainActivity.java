b42a7b6cf86981bde5026c397cb1f3bc39b4036d
Ziyun Ge <ziyunge1999@gmail.com>
2022-05-04 03:18:23 -0700

    finish auto complete

--- app/src/main/java/com/example/stocks/MainActivity.java
+++ app/src/main/java/com/example/stocks/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.stocks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,26 +12,41 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.ItemTouchHelper;
 
+import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.Time;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
@@ -61,6 +77,11 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences shareCostPref;
     SharedPreferences.Editor shareCostEditor;
 
+//    Long lastChangeTimestamp;
+    final String basicUrl = "https://stocksearchnodejs.wl.r.appspot.com/api/v1/";
+    RequestQueue queue;
+    String currentStringOnSearch = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.e("gzy", "Create Main Activity");
@@ -68,6 +89,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         df = new DecimalFormat("0.00");
+//        lastChangeTimestamp = System.currentTimeMillis() / 1000L;
+        queue = Volley.newRequestQueue(this);
 
         recyclerView = findViewById(R.id.recyclerView);
         favoritesRecyclerView = findViewById(R.id.favoritesRecyclerView);
@@ -141,6 +164,7 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+    @SuppressLint("RestrictedApi")
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -156,7 +180,77 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String s) {
+                s = s.toUpperCase();
+                currentStringOnSearch = s;
                 Log.e("gzy", "onQueryTextChange: " + s);
+//                Long currentTimestamp = System.currentTimeMillis() / 1000L;
+                if (s.length() > 1) {
+                    final String searchS = s;
+                    new android.os.Handler(Looper.getMainLooper()).postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    Log.e("gzy", "1 second times up");
+                                    if (!searchS.equals(currentStringOnSearch)) {
+                                        return;
+                                    }
+                                    String url = basicUrl + "search?q=" + searchS;
+                                    Log.e("gzy", "auto complete request send: " + url);
+                                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                                            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                                                @Override
+                                                public void onResponse(JSONObject response) {
+                                                    Log.e("gzy", "auto complete request returns: " + url);
+                                                    try {
+                                                        JSONArray result = response.getJSONArray("result");
+                                                        ArrayList<String> autocompleteResult = new ArrayList<>();
+                                                        for (int i = 0; i < result.length(); i++) {
+                                                            JSONObject item = result.getJSONObject(i);
+                                                            String symbol = item.getString("symbol");
+                                                            String fullname = item.getString("description");
+                                                            if (!symbol.matches(".*\\d.*") && !symbol.contains(".")) {
+                                                                String cur = symbol + " | " + fullname;
+                                                                autocompleteResult.add(cur);
+                                                            }
+                                                        }
+                                                        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete)searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+                                                        searchAutoComplete.setDropDownBackgroundResource(R.color.white);
+                                                        searchAutoComplete.setThreshold(1);
+                                                        String dataArr[] = autocompleteResult.toArray(new String[autocompleteResult.size()]);
+//                                        String dataArr[] = {"Apple" , "Amazon" , "Amd", "Microsoft", "Microwave", "MicroNews", "Intel", "Intelligence"};
+                                                        ArrayAdapter<String> newsAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, dataArr);
+                                                        searchAutoComplete.setAdapter(newsAdapter);
+                                                        searchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                                            @Override
+                                                            public void onItemClick(AdapterView<?> adapterView, View view, int itemIndex, long id) {
+                                                                String queryString = adapterView.getItemAtPosition(itemIndex).toString();
+//                                                                searchAutoComplete.setText("" + queryString);
+//                                                                Toast.makeText(MainActivity.this, "you clicked " + queryString, Toast.LENGTH_SHORT).show();
+                                                                String selectedTicker = queryString.substring(0, queryString.indexOf(" | "));
+                                                                Intent intent = new Intent(MainActivity.this, DisplayStockInfo.class);
+                                                                intent.putExtra(EXTRA_MESSAGE, selectedTicker);
+                                                                intent.putExtra(EXTRA_PARENT_KEY, "MainActivity");
+                                                                startActivity(intent);
+                                                                finish();
+                                                            }
+                                                        });
+                                                    } catch (JSONException e) {
+                                                        e.printStackTrace();
+                                                    }
+                                                }
+                                            }, new Response.ErrorListener() {
+
+                                                @Override
+                                                public void onErrorResponse(VolleyError error) {
+
+                                                }
+                                            });
+                                    // Add the request to the RequestQueue.
+                                    queue.add(jsonObjectRequest);
+                                }
+                            },
+                            1000);
+                }
                 return true;
             }
         });
